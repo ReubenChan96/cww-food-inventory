f6ed192b6c2c40521724473b592594a4b010316b
ReubenChan96 <reubenc24@hotmail.com>
2025-10-05 09:21:39 +0800

    added vercel/speed-insights

--- src/main.tsx
+++ src/main.tsx
@@ -5,3 +5,22 @@
 
   createRoot(document.getElementById("root")!).render(<App />);
   
+  import { SpeedInsights } from '@vercel/speed-insights/next';
+
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <html lang="en">
+        <head>
+          <title>Next.js</title>
+        </head>
+        <body>
+          {children}
+          <SpeedInsights />
+        </body>
+      </html>
+    );
+  }
